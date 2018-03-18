e334481f43279696c79eb4c06f598f045d275af3
tharaney <37337120+tharaney@users.noreply.github.com>
2018-03-18 20:23:17 +0530

    Update odd even.py

--- Beginner level/odd even.py
+++ Beginner level/odd even.py
@@ -1,5 +1,9 @@
 a=int(raw_input())
-if(a%2==1):
-	print('Odd')
+print a
+if (a>=1 and a<=100000):
+	if(a%2==1):
+		print('Odd')
+	else:
+		print('Even')
 else:
-	print('Even')
+	print('invalid input')
